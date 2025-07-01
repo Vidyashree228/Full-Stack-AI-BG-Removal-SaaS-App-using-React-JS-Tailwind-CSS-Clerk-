55a9fa2aaf197f613c3bd9e0ea422bb4516e35cd
Vidyashree228 <vidyamanju68@gmail.com>
2025-07-01 09:59:37 +0530

    Trigger webhook deployment

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import connectDB from "./configs/mongodb.js";
+import useRouter from "./routes/userRoute.js";
+// import imageRouter from "./routes/imagesRoutes.js";
+
 
 const PORT = process.env.PORT || 4000;
 const app = express();
@@ -12,5 +15,6 @@ await connectDB()
 
 
 app.get("/", (req, res) => res.send("API is working"));
-
+app.use("/api/user", useRouter);
+// app.use("/api/image", imageRouter);
 app.listen(PORT, () => console.log("Server running on port: ", PORT));
